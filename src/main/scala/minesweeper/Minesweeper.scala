a5cd08f2ecf247397e53f7506f2eae7691efe97d
kurochan <kuro@kurochan.org>
2014-12-29 00:36:20 +0900

    地雷の配置のランダム化

--- src/main/scala/minesweeper/Minesweeper.scala
+++ src/main/scala/minesweeper/Minesweeper.scala
@@ -1,7 +1,15 @@
 package minesweeper
 
+import scala.util.Random
+import scala.annotation.tailrec
+
 case class Minesweeper(sizeRow: Int, sizeCol: Int, mineNum: Int) {
-  private val mines = Set(1, 5, 6)
+  private val mines: Set[Int] = {
+    @tailrec
+    def mkMines(set: Set[Int] = Set()): Set[Int] =
+      if (set.size != mineNum) mkMines(set + Random.nextInt % sizeRow * sizeCol) else set
+    mkMines()
+  }
   private val field: Seq[Seq[Area]] = (0 to sizeRow - 1).map(
     row => (0 to sizeCol - 1).map(col =>
       if (mines(row * sizeCol + col)) MineArea(this, row, col) else NormalArea(this, row, col)))
