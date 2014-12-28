14c8156175d8273aa506f8c861c14d21ba6a2310
kurochan <kuro@kurochan.org>
2014-12-29 00:07:36 +0900

    mainが値を戻さないように変更

--- src/main/scala/minesweeper/Main.scala
+++ src/main/scala/minesweeper/Main.scala
@@ -3,7 +3,7 @@ package minesweeper
 import minesweeper.ui.cui.CUI
 
 object Main {
-  def main(args: Array[String]): Unit = {
+  def main(args: Array[String]) {
     val ms = Minesweeper(5, 5, 5)
     CUI(ms).attach
   }
