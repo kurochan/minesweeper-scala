6b3b98a04acb7ed16e18771682731f23ab3d8449
kurochan <kuro@kurochan.org>
2014-12-29 00:51:46 +0900

    ランダム配置の方法変更

--- src/main/scala/minesweeper/Minesweeper.scala
+++ src/main/scala/minesweeper/Minesweeper.scala
@@ -4,12 +4,8 @@ import scala.util.Random
 import scala.annotation.tailrec
 
 case class Minesweeper(sizeRow: Int, sizeCol: Int, mineNum: Int) {
-  private val mines: Set[Int] = {
-    @tailrec
-    def mkMines(set: Set[Int] = Set()): Set[Int] =
-      if (set.size != mineNum) mkMines(set + Random.nextInt % sizeRow * sizeCol) else set
-    mkMines()
-  }
+  private val mines: Set[Int] =
+    Random.shuffle(0 to sizeRow * sizeCol - 1).slice(0, mineNum).toSet
   private val field: Seq[Seq[Area]] = (0 to sizeRow - 1).map(
     row => (0 to sizeCol - 1).map(col =>
       if (mines(row * sizeCol + col)) MineArea(this, row, col) else NormalArea(this, row, col)))
