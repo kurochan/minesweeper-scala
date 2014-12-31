6f20199bdd4b488dc30a585e2d9a8d5213b8beaa
kurochan <kuro@kurochan.org>
2015-01-01 04:39:31 +0900

    ステートを表すオブジェクトを作成

--- src/main/scala/minesweeper/Minesweeper.scala
+++ src/main/scala/minesweeper/Minesweeper.scala
@@ -3,7 +3,21 @@ package minesweeper
 import scala.util.Random
 import scala.annotation.tailrec
 
-case class Minesweeper(sizeRow: Int, sizeCol: Int, mineNum: Int) {
+sealed abstract class Minesweeper() {
+  val sizeRow: Int
+  val sizeCol: Int
+  def apply(row: Int)(col: Int): Area
+}
+
+object Minesweeper {
+
+  def apply(sizeRow: Int, sizeCol: Int, mineNum: Int) = {
+    new Playing(sizeRow, sizeCol, mineNum)
+  }
+}
+
+class Playing(val sizeRow: Int, val sizeCol: Int, mineNum: Int) extends
+  Minesweeper {
 
   private val mines: Set[Int] =
     Random.shuffle(0 to sizeRow * sizeCol - 1).slice(0, mineNum).toSet
@@ -14,3 +28,12 @@ case class Minesweeper(sizeRow: Int, sizeCol: Int, mineNum: Int) {
 
   def apply(row: Int)(col: Int): Area = field(row)(col)
 }
+
+class Cleared(ms: Minesweeper) extends Minesweeper {
+  val sizeRow = ms.sizeRow
+  val sizeCol = ms.sizeRow
+
+  def apply(row: Int)(col: Int): Area = ms(row)(col)
+}
+
+class Dead(ms: Playing) extends Cleared(ms: Playing)
