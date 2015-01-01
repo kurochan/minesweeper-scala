5f858569ec20a8c600304b9328c29917c7d61522
kurochan <kuro@kurochan.org>
2015-01-01 19:17:18 +0900

    開かれているマス数を記憶するように

--- src/main/scala/minesweeper/Minesweeper.scala
+++ src/main/scala/minesweeper/Minesweeper.scala
@@ -6,6 +6,7 @@ import scala.annotation.tailrec
 sealed abstract class Minesweeper() {
   val sizeRow: Int
   val sizeCol: Int
+  val countOpen: Int
   def apply(row: Int)(col: Int): Area
 }
 
@@ -15,11 +16,11 @@ object Minesweeper {
     Random.shuffle(0 to size - 1).slice(0, mineNum).toSet
 
   def apply(sizeRow: Int, sizeCol: Int, mineNum: Int) = {
-    new Playing(sizeRow, sizeCol, initMines(sizeRow * sizeCol, mineNum))
+    new Playing(sizeRow, sizeCol, 0, initMines(sizeRow * sizeCol, mineNum))
   }
 }
 
-class Playing(val sizeRow: Int, val sizeCol: Int,
+class Playing(val sizeRow: Int, val sizeCol: Int, val countOpen: Int,
   mines: Set[Int], prevField: Option[Seq[Seq[Area]]] = None) extends Minesweeper {
 
   private val field: Seq[Seq[Area]] = prevField match {
@@ -34,10 +35,10 @@ class Playing(val sizeRow: Int, val sizeCol: Int,
   def apply(row: Int)(col: Int): Area = field(row)(col)
 }
 
-class Cleared(override val sizeRow: Int, override val sizeCol: Int,
+class Cleared(override val sizeRow: Int, override val sizeCol: Int, countOpen: Int,
   mines: Set[Int], prevField: Option[Seq[Seq[Area]]] = None) extends
-    Playing(sizeRow: Int, sizeCol: Int, mines: Set[Int], prevField: Option[Seq[Seq[Area]]])
+    Playing(sizeRow: Int, sizeCol: Int, countOpen: Int, mines: Set[Int], prevField: Option[Seq[Seq[Area]]])
 
-class Dead(override val sizeRow: Int, override val sizeCol: Int,
+class Dead(override val sizeRow: Int, override val sizeCol: Int, countOpen: Int,
   mines: Set[Int], prevField: Option[Seq[Seq[Area]]] = None) extends
-    Playing(sizeRow: Int, sizeCol: Int, mines: Set[Int], prevField: Option[Seq[Seq[Area]]])
+    Playing(sizeRow: Int, sizeCol: Int, countOpen: Int, mines: Set[Int], prevField: Option[Seq[Seq[Area]]])
